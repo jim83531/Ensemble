0e7bba1d942f74a27b4035e83e44e2e6603ca01e
Sider <jim83531@gmail.com>
2017-12-25 17:20:56 +0800

    吼吼吼

--- ensemble_test.py
+++ ensemble_test.py
@@ -14,16 +14,16 @@ import sklearn.model_selection
 import sklearn.linear_model
 import sklearn.preprocessing
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import LinearSVC
+from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 
 
 def load_train_test_data(train_ratio=.5):
-    df = pandas.read_csv('./creditcard.csv')
+    df = pandas.read_csv('./dadada.csv')
     data = df.values
-    X = numpy.delete(data,30,1)
-    X = numpy.concatenate((numpy.ones((len(X),1)),X), axis=1)
-    y = data[:,-1].reshape(-1,1)
+    numpy.random.shuffle(data)
+    X = numpy.delete(data,1,1)
+    y = data[:,-1]
     return sklearn.model_selection.train_test_split(X, y, test_size = 1 - train_ratio, random_state=0)
 
 def scale_features(X_train, X_test, low=0, upp=1):
@@ -32,23 +32,24 @@ def scale_features(X_train, X_test, low=0, upp=1):
     X_test_scale = minmax_scaler.transform(X_test)
     return X_train_scale, X_test_scale
 
-def knn(X_train, y_train, x):
-    knn = KNeighborsClassifier(n_neighbors=3)
+def knn(X_train, y_train, X_test):
+    knn = KNeighborsClassifier(n_neighbors=9,n_jobs=-1)
     knn.fit(X_train,y_train)
     
-    return knn.predict_proba(x),knn.predict(x)
+    return knn.predict(X_test), knn.predict_proba(X_test)
     
-def logreg(X_train, y_train, x):
-    logreg =LinearSVC(C=1.0, max_iter=1000, penalty='l2', random_state=0, tol=0.0001 )
+def logreg(X_train, y_train, X_test):
+    logreg =LogisticRegression()
     logreg.fit(X_train, y_train)
-
-    return 1/(1+numpy.exp(-(numpy.dot(x,logreg.coef_.T)))) , logreg.predict(x)
     
-def dectree(X_train, y_train, x):
+    return logreg.predict(X_test), logreg.predict_proba(X_test)
+#1/(1+numpy.exp(-(numpy.dot(X_test,logreg.coef_.T))))
+    
+def dectree(X_train, y_train, X_test):
     dectree = DecisionTreeClassifier(random_state=0)
     dectree.fit(X_train, y_train)
 
-    return dectree.predict_proba(x), dectree.predict(x)
+    return  dectree.predict(X_test), dectree.predict_proba(X_test)
 
 def ensemble_feaure_loss(X_train,y_train,X_test,y_test):
     n,d = X_train.shape
@@ -60,14 +61,7 @@ def ensemble_feaure_loss(X_train,y_train,X_test,y_test):
         a,a_out = knn(X_train,y_train,x)
         b,b_out = logreg(X_train,y_train,x)
         c,c_out = dectree(X_train,y_train,x)
-        if (y_test[i] == 0):
-            a_loss = a[0,0]
-            b_loss = (1-b)
-            c_loss = c[0,0]
-        if (y_test[i] == 1):
-            a_loss = a[0,1]
-            b_loss = b
-            c_loss = c[0,1]
+        
         wa = a_loss
         wb = b_loss
         wc = c_loss 
@@ -77,11 +71,19 @@ def ensemble_feaure_loss(X_train,y_train,X_test,y_test):
     return enp
 
 def main(argv):
-    X_train, X_test, y_train, y_test = load_train_test_data(train_ratio=.8)
+    X_train, X_test, y_train, y_test = load_train_test_data(train_ratio=.7)
     X_train_scale, X_test_scale = scale_features(X_train, X_test, 0, 1)
     
-    ans = ensemble_feaure_loss(X_train,y_train,X_test,y_test)
-    print("Linear test R^2: %f" % (sklearn.metrics.r2_score(y_test, ans)))
+    #ans = ensemble_feaure_loss(X_train,y_train,X_test,y_test)
+    kn,knp=knn(X_train,y_train,X_test)
+    print(knp)
+    print("knn test R^2: %f" % (sklearn.metrics.r2_score(y_test, kn)))
+    log,logp=logreg(X_train,y_train,X_test)
+    print(logp)
+    print("log test R^2: %f" % (sklearn.metrics.r2_score(y_test, log)))
+    dec,decp=dectree(X_train,y_train,X_test)
+    print(decp)
+    print("dec test R^2: %f" % (sklearn.metrics.r2_score(y_test, dec)))
     
 
 if __name__ == "__main__":
