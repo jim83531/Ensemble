bb98eb3ac02f06c5ea020aefcf3e67d9c0b59148
Sider <jim83531@gmail.com>
2018-01-15 15:09:09 +0800

    最新版
    更改過 loss 計算
    目前要生data 來測試其效果
    還有要 看 model calibrate

--- nnensemble.py
+++ nnensemble.py
@@ -16,58 +16,103 @@ import sklearn.preprocessing
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
+#from sklearn.datasets.samples_generator import make_swiss_roll
+
 
 def manipulate_data():
-    X1_data = np.random.randint(50,size=250)
-    X2_data = np.random.randint(50,size=250)
+    '''
+    X1_data = 10 + np.random.randint(15,size=250)
+    #X11_data = np.random.randint(60,size=250)
+    X2_data = 10 + np.random.randint(15,size=250)
+    #X22_data = np.random.randint(60,size=250)
 
-    X1_b_data = X1_data - np.random.randint(25,size=250)
-    X2_b_data = X2_data - np.random.randint(25,size=250)
 
-    X_data = np.vstack((np.hstack((X1_data,X1_b_data)),np.hstack((X2_data,X2_b_data))))
-    y_data = np.hstack((np.ones(250),np.zeros(250)))
-    y_data = y_data.T.reshape(1,500)
-    data = np.vstack((X_data,y_data))
-    data = data.T
-    values = data
+    X1_b_data = 18 + np.random.randint(15,size=250)
+    #X11_b_data = X11_data + np.random.randint(30,size=250)
+    X2_b_data = 18 + np.random.randint(15,size=250)
+    #X22_b_data = X22_data + np.random.randint(30,size=250)
+
+
+    a = np.hstack((X1_data,X2_data))
+    #b = np.hstack((X11_data,X1_b_data))
+    c = np.hstack((X1_b_data,X2_b_data))
+    #d = np.hstack((X22_data,X2_b_data))
+    #ab = np.hstack((a,b))
+    #cd = np.hstack((c,d))
+    X_data = np.vstack((a,c))
+    
+    print(X_data.shape)
+    '''
+    n_samples = 1000
+    t_0 = 1.5 * np.pi * (1 + 1.5 * np.random.rand(1, n_samples))
+    t_1 = 1.5 * np.pi * (2.5 + 1.5 * np.random.rand(1, n_samples))
+    x_0 = t_0 * np.cos(t_0)
+    y_0 = t_0 * np.sin(t_0)
+    X_0 = np.concatenate((x_0, y_0))
+    X_0 += .7 * np.random.randn(2, n_samples)
+    x_1 = t_1 * np.cos(t_1)
+    y_1 = t_1 * np.sin(t_1)
+    X_1 = np.concatenate((x_1, y_1))
+    X_1 += .7 * np.random.randn(2, n_samples)
+    
+    X = np.hstack((X_0, X_1))
+
+    X_data = X.T
+
+    y_data = np.hstack((np.hstack((np.ones(1000),np.zeros(1000)))))
+
+
+    y_data = y_data.T.reshape(2000,1)
+    print(X_data.shape)
+    print(y_data.shape)
+    
+    data = np.hstack((X_data, y_data))
+    #data = data.T
+
+    '''values = data
     df = pd.DataFrame(values)
     df.to_csv("dadada.csv", sep=',')
+    
+    df = pd.read_csv('./dadada.csv')
+    data = df.values'''
     np.random.shuffle(data)
-    X = np.delete(data,1,1)
+    X = np.delete(data,-1,1)
     y = data[:,-1]
+    #print(y)'''
+
     return data,X,y
 
 def load_train_test_data(X,y,train_ratio=.5):
     
     return sklearn.model_selection.train_test_split(X, y, test_size = 1 - train_ratio, random_state=0)
 
-def scale_features(X_train, X_test, low=0, upp=1):
-    minmax_scaler = sklearn.preprocessing.MinMaxScaler(feature_range=(low, upp)).fit(np.vstack((X_train, X_test)))
-    X_train_scale = minmax_scaler.transform(X_train)
-    X_test_scale = minmax_scaler.transform(X_test)
-    return X_train_scale, X_test_scale
+def scale_features(X, low=0, upp=1):
+    minmax_scaler = sklearn.preprocessing.MinMaxScaler(feature_range=(low, upp)).fit(X)
+    X_scale = minmax_scaler.transform(X)
+
+    return X_scale
 
 def knn(X_train, y_train, X_test):
     knn = KNeighborsClassifier(n_neighbors=9,n_jobs=-1)
     knn.fit(X_train,y_train)
     
-    return knn.predict(X_test), knn.predict_proba(X_test), knn.predict_proba(X_train)
+    return knn.predict(X_test), knn.predict_proba(X_train), knn.predict_proba(X_test)
     
 def logreg(X_train, y_train, X_test):
     logreg =LogisticRegression()
     logreg.fit(X_train, y_train)
     
-    return logreg.predict(X_test), logreg.predict_proba(X_test), logreg.predict_proba(X_train)
+    return logreg.predict(X_test), logreg.predict_proba(X_train), logreg.predict_proba(X_test)
     #1/(1+numpy.exp(-(numpy.dot(X_test,logreg.coef_.T))))
     
 def dectree(X_train, y_train, X_test):
     dectree = DecisionTreeClassifier(random_state=0)
     dectree.fit(X_train, y_train)
 
-    return  dectree.predict(X_test), dectree.predict_proba(X_test), dectree.predict_proba(X_train)
+    return  dectree.predict(X_test), dectree.predict_proba(X_train), dectree.predict_proba(X_test)
 
 def add_layer(inputs, in_size, out_size, activation_function=None):
-    Weights = tf.Variable(tf.random_normal([in_size, out_size]))
+    Weights = tf.Variable(tf.zeros([in_size, out_size]))
     biases = tf.Variable(tf.zeros([1, out_size]) + 0.01)
     Wx_plus_b = tf.matmul(inputs, Weights) + biases 
     
@@ -79,49 +124,155 @@ def add_layer(inputs, in_size, out_size, activation_function=None):
 def nn(X,y,X_t):
     xs = tf.placeholder(tf.float32, [None, 2])
     ys = tf.placeholder(tf.float32, [None, 3])
-    l1 = add_layer(xs, 2, 10, activation_function=tf.sigmoid)
-    prediction = add_layer(l1, 10, 3, activation_function=tf.sigmoid)
+    l1 = add_layer(xs, 2, 25, activation_function = tf.nn.sigmoid)
+    #l2 = add_layer(l1, 25, 30, activation_function=tf.nn.relu)
+    #l3 = add_layer(l2, 30, 15, activation_function=tf.nn.sigmoid)
+    prediction = add_layer(l1, 25, 3, activation_function= None)
     x_data = X.astype(np.float32)
     y_data = y.astype(np.float32)
+    #print(y_data)
     
     loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction)))
-    train_step = tf.train.AdadeltaOptimizer(0.5).minimize(loss)
+    train_step = tf.train.AdadeltaOptimizer(0.1).minimize(loss)
     init = tf.initialize_all_variables()
     sess = tf.Session()
     sess.run(init)
     
     
     
-    for i in range(50000):
+    for i in range(30000):
         # 整個訓練最核心的code , feed_dict 表示餵入 輸入與輸出
         sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
         print(sess.run(loss, feed_dict={xs: x_data, ys: y_data}))
     
+    print(sess.run(loss, feed_dict={xs: x_data, ys: y_data}))
     prediction_value = sess.run(prediction, feed_dict={xs: X_t})        
     return  prediction_value
     
+def mv(kn,log,dec):
+    
+    n,d = kn.shape
+    mvm = np.zeros((n,1))
+    zcount = np.zeros((n,1))
+    ocount = np.zeros((n,1))
+    for i in range(n) :
+        if (kn[i]==0):
+            zcount[i] += 1
+        else:
+            ocount[i] += 1
+        if (log[i]==0):
+            zcount[i] += 1
+        else:
+            ocount[i] += 1
+        if (dec[i]==0):
+            zcount[i] += 1
+        else:
+            ocount[i] += 1    
+        
+        if (zcount[i] > ocount[i]):
+            mvm[i]=0
+        else:
+            mvm[i]=1
+    
+    return mvm
+
+def en(nnyp,my):
+    
+
+    
+    n,d =my.shape
+    en_p = np.zeros((n,1))
+    w = np.zeros((1,3))
+
+
+    
+    for i in range(n):
+        summ = np.sum(1/nnyp[i])
+        w = (1/nnyp)/summ
+        en_p[i] = np.dot(w[i].T.reshape(1,3),my[i].reshape(3,1))
+
+        if (en_p[i]>0.5):
+            en_p[i]=1
+        else:
+            en_p[i]=0
+    
+    return en_p
 
 def main(argv):
     data,X,y = manipulate_data()
-    X_train, X_test, y_train, y_test = load_train_test_data(X,y,train_ratio=.7)
-    X_train_scale, X_test_scale = scale_features(X_train, X_test, 0, 1)
     
-    kn,knp_te,knp_tr=knn(X_train_scale,y_train,X_test_scale)
-    knp_te = np.delete(knp_te,1,1)
-    knp_tr = np.delete(knp_tr,1,1)
-    #print("knn test R^2: %f" % (sklearn.metrics.r2_score(y_test, kn)))
-    log,logp_te,logp_tr=logreg(X_train_scale,y_train,X_test_scale)
-    logp_te = np.delete(logp_te,1,1)
-    logp_tr = np.delete(logp_tr,1,1)
-    #print("log test R^2: %f" % (sklearn.metrics.r2_score(y_test, log)))
-    dec,decp_te,decp_tr=dectree(X_train_scale,y_train,X_test_scale)
-    decp_te = np.delete(decp_te,1,1)
-    decp_tr = np.delete(decp_tr,1,1)
-    #print("dec test R^2: %f" % (sklearn.metrics.r2_score(y_test, dec)))
-    y_1_p_te =np.hstack((np.hstack((knp_te,logp_te)),decp_te)) 
-    y_1_p_tr =np.hstack((np.hstack((knp_tr,logp_tr)),decp_tr)) 
+    X_scale = scale_features(X, 0, 1)
+    X_train, X_test, y_train, y_test = load_train_test_data(X_scale,y,train_ratio=.7)
+    y_test.reshape(-1,1)
+    y_train.reshape(-1,1)
+    m,k = X_train.shape
+    n,d = X_test.shape
     
-    yp_tr = nn(X_test_scale,y_1_p_te,X_train_scale)
-    print("nn_p test R^2: %f" % (sklearn.metrics.r2_score(yp_tr, y_1_p_tr)))
+    
+    for i in range(n):
+        if (y_test[i] ==1):
+            plt.scatter(X_test[i,0],X_test[i,1], c = 'g')
+        else:
+            plt.scatter(X_test[i,0],X_test[i,1], c = 'r')
+    
+    kn,knp_tr,knp_te=knn(X_train,y_train,X_test)
+    kn = kn.reshape(n,1)
+    kn_tr_l =np.zeros((m,1))
+    knp_te_0 = np.delete(knp_te,1,1)
+    knp_tr_0 = np.delete(knp_tr,1,1)
+    knp_te_1 = np.delete(knp_te,0,1)
+    knp_tr_1 = np.delete(knp_tr,0,1)
+    
+    
+    
+    log,logp_tr,logp_te=logreg(X_train,y_train,X_test)
+    log = log.reshape(n,1)
+    log_tr_l =np.zeros((m,1))
+    logp_te_0 = np.delete(logp_te,1,1)
+    logp_tr_0 = np.delete(logp_tr,1,1)
+    logp_te_1 = np.delete(logp_te,0,1)
+    logp_tr_1 = np.delete(logp_tr,0,1)
+    
+    
+    dec,decp_tr,decp_te=dectree(X_train,y_train,X_test)
+    dec = dec.reshape(n,1)
+    dec_tr_l =np.zeros((m,1))
+    decp_te_0 = np.delete(decp_te,1,1)
+    decp_tr_0 = np.delete(decp_tr,1,1)
+    decp_te_1 = np.delete(decp_te,0,1)
+    decp_tr_1 = np.delete(decp_tr,0,1)
+    
+    
+    
+    for i in range(m):
+        kn_tr_l[i] = (y_train[i] - knp_tr_1[i])
+        log_tr_l[i] = (y_train[i] - logp_tr_1[i])
+        dec_tr_l[i] = (y_train[i] - decp_tr_1[i])
+        
+
+    y_hat = np.hstack((np.hstack((kn,log)),dec))
+    y_1_p_te =np.hstack((np.hstack((knp_te_1,logp_te_1)),decp_te_1))
+    y_0_p_te =np.hstack((np.hstack((knp_te_0,logp_te_0)),decp_te_0))
+    y_1_p_tr =np.hstack((np.hstack((knp_tr_1,logp_tr_1)),decp_tr_1))
+    y_0_p_tr =np.hstack((np.hstack((knp_tr_0,logp_tr_0)),decp_tr_0))
+    yp_te = np.hstack((y_1_p_te,y_0_p_te))
+    yp_tr_l = np.hstack((np.hstack((kn_tr_l,log_tr_l)),dec_tr_l))
+    print(yp_tr_l)
+    nn_yp_te = nn(X_train,yp_tr_l,X_test)
+    #print(nn_yp_te)
+
+    mvm = mv(kn,log,dec)
+    #print(y)
+
+    en_p = en(nn_yp_te,y_1_p_te)
+    
+    print("knn test : %f" % (sklearn.metrics.accuracy_score(kn, y_test)))
+    print("log test : %f" % (sklearn.metrics.accuracy_score(log, y_test)))
+    print("dec test : %f" % (sklearn.metrics.accuracy_score(dec, y_test)))
+    #print("nn_p test R^2: %f" % (sklearn.metrics.r2_score(nn_yp_te, yp_te)))
+    print("mv test : %f" % (sklearn.metrics.accuracy_score(mvm,y_test)))
+    print("en_p test : %f" % (sklearn.metrics.accuracy_score(en_p,y_test)))
+
+    
 if __name__ == "__main__":
     main(sys.argv)
